227e002472ab91ba09b61c68e48f756235d23dbf
santhoshkumarl7 <126152530+santhoshkumarl7@users.noreply.github.com>
2023-05-01 21:00:17 +0530

    Update index.jsx

--- src/components/Header/SearchForm/index.jsx
+++ src/components/Header/SearchForm/index.jsx
@@ -6,6 +6,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import './SearchForm.css';
 
+
 export default function SearchForm(props) {
   const { active } = props;
   return (
